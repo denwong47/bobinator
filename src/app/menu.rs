52f477502577e9fa45a9e1966c165af105fb2644
Denny Wong <denwong47@hotmail.com>
2023-03-15 00:27:58 +0000

    Sample script to run requests with tokio::spawn.

--- src/app/menu.rs
+++ src/app/menu.rs
@@ -1,9 +1,9 @@
+use futures::stream::{FuturesOrdered, StreamExt};
 use lazy_static::lazy_static;
 use reqwest::Client;
+use std::sync::Arc;
 
 use conch;
-
-use chrono::{offset::Local, Duration};
 
 use crate::common::*;
 use crate::*;
@@ -17,7 +17,7 @@ lazy_static! {
 }
 
 /// Show main loop menu for commands.
-pub async fn menu(conn: &Client, session: &LoginSession) -> Result<(), BobinatorError> {
+pub async fn menu(conn: Arc<Client>, session: &LoginSession) -> Result<(), BobinatorError> {
     loop {
         println!("\n{}", MENU_PROMPT.to_string());
 
@@ -28,16 +28,41 @@ pub async fn menu(conn: &Client, session: &LoginSession) -> Result<(), Bobinator
             },
         ) {
             // UserInput::Integer(0) => app::timeoff::legacy_book_fridays_off(conn, session).await?,
-            UserInput::Char('0') => app::timeoff::timeoff_dashboard(conn, session).await?,
+            UserInput::Char('0') => {
+                app::timeoff::timeoff_dashboard(&Arc::clone(&conn), session).await?
+            }
             UserInput::Char('1') => {
-                let off_today = bob::cookies::timeoff::absentees_on(
-                    conn,
-                    Local::now().date_naive() + Duration::days(3),
-                )
-                .await?;
+                println!("Off This Friday:");
 
-                for timeoff in off_today.iter_requests() {
-                    println!("{:?}", timeoff.employee_id.enquire_employee(conn).await)
+                let off_today = Arc::new(
+                    bob::cookies::timeoff::absentees_on(&Arc::clone(&conn), FridayOff::this_week())
+                        .await?,
+                );
+
+                let mut tasks = FuturesOrdered::new();
+
+                off_today.iter_requests().for_each(|timeoff| {
+                    let conn_local = Arc::clone(&conn);
+                    let employee_id = timeoff.employee_id().to_string();
+
+                    tasks.push_back(tokio::spawn(async move {
+                        employee_id.enquire_employee(&conn_local).await
+                    }))
+                });
+
+                let mut timeoffs = off_today.iter_requests();
+                while let Some(output) = tasks.next().await {
+                    if let Some(timeoff) = timeoffs.next() {
+                        output
+                            .map_err(|err| BobinatorError::AsyncJoinError(err))
+                            .and_then(|result| result)
+                            .map(|employee| {
+                                println!(
+                                    "{} {} ({})",
+                                    timeoff, employee.display_name, employee.work.department
+                                )
+                            })?;
+                    }
                 }
             }
             UserInput::Exit => break,
